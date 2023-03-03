bb7b88287dd82e009c5402b3672c899d171b1146
Rcom Dat <rcomdat28.3@gmail.com>
2023-03-03 11:41:07 +0700

    Release suntech frontend final project

---
+++ assets/js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+        name:"BlueTequila",
+        artist: "Táo",
+        img: "baron.png",
+        src: "music-1"
+    },
+    {
+        name:"thế giới có nơi nào cho em",
+        artist: "Unknown",
+        img: "baron.png",
+        src: "music-2"
+    },
+    {
+        name:"lơ mơ",
+        artist: "mer",
+        img:"baron.png",
+        src: "music-3"
+    }
+];
